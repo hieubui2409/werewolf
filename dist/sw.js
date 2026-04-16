ffdfd86ff1294fc3551f490814f304c7535cd8d0
Werewolf Dev Team <dev@werewolf.local>
2026-04-16 16:06:10 +0700

    chore(dist): update build artifacts with new bundle and self-hosted fonts

--- dist/sw.js
+++ dist/sw.js
@@ -1,1 +1,86 @@
-if(!self.define){let e,s={};const i=(i,n)=>(i=new URL(i+".js",n).href,s[i]||new Promise(s=>{if("document"in self){const e=document.createElement("script");e.src=i,e.onload=s,document.head.appendChild(e)}else e=i,importScripts(i),s()}).then(()=>{let e=s[i];if(!e)throw new Error(`Module ${i} didn’t register its module`);return e}));self.define=(n,r)=>{const o=e||("document"in self?document.currentScript.src:"")||location.href;if(s[o])return;let d={};const l=e=>i(e,o),t={module:{uri:o},exports:d,require:l};s[o]=Promise.all(n.map(e=>t[e]||l(e))).then(e=>(r(...e),d))}}define(["./workbox-1ef09536"],function(e){"use strict";self.addEventListener("message",e=>{e.data&&"SKIP_WAITING"===e.data.type&&self.skipWaiting()}),e.precacheAndRoute([{url:"index.html",revision:"a9b57134d55e326de3f7f49a8ecdf2e0"},{url:"icons.svg",revision:"3b4fcfcf393eca4d264dca4a4663bc37"},{url:"favicon.svg",revision:"7e840862161341271697daa99a40d76b"},{url:"sounds/timer-tick.mp3",revision:"5fbc2923819f0f1f8b0e12c03179cea5"},{url:"sounds/timer-end.mp3",revision:"abef5396ebc02ca00848f92d6cd1e6c6"},{url:"sounds/night-ambience.mp3",revision:"db96d1db010d11de4e3a784c7cb349fc"},{url:"assets/workbox-window.prod.es5-Bq4GJJid.js",revision:null},{url:"assets/index-C4mTyk5H.js",revision:null},{url:"assets/index-Bj5Z4-hw.css",revision:null},{url:"assets/fa-solid-900-DRAAbZTg.woff2",revision:null},{url:"assets/fa-regular-400-nyy7hhHF.woff2",revision:null},{url:"assets/fa-brands-400-BP5tdqmh.woff2",revision:null},{url:"manifest.webmanifest",revision:"472d9d354c8e34d3947636d64100d3a2"}],{}),e.cleanupOutdatedCaches(),e.registerRoute(new e.NavigationRoute(e.createHandlerBoundToURL("index.html")))});
+if (!self.define) {
+  let e,
+    i = {};
+  const s = (s, n) => (
+    (s = new URL(s + ".js", n).href),
+    i[s] ||
+      new Promise((i) => {
+        if ("document" in self) {
+          const e = document.createElement("script");
+          ((e.src = s), (e.onload = i), document.head.appendChild(e));
+        } else ((e = s), importScripts(s), i());
+      }).then(() => {
+        let e = i[s];
+        if (!e) throw new Error(`Module ${s} didn’t register its module`);
+        return e;
+      })
+  );
+  self.define = (n, r) => {
+    const o =
+      e ||
+      ("document" in self ? document.currentScript.src : "") ||
+      location.href;
+    if (i[o]) return;
+    let f = {};
+    const t = (e) => s(e, o),
+      d = { module: { uri: o }, exports: f, require: t };
+    i[o] = Promise.all(n.map((e) => d[e] || t(e))).then((e) => (r(...e), f));
+  };
+}
+define(["./workbox-1ef09536"], function (e) {
+  "use strict";
+  (self.addEventListener("message", (e) => {
+    e.data && "SKIP_WAITING" === e.data.type && self.skipWaiting();
+  }),
+    e.precacheAndRoute(
+      [
+        { url: "index.html", revision: "bf01764be9ebf1b542aa2b73ffda8443" },
+        { url: "icons.svg", revision: "3b4fcfcf393eca4d264dca4a4663bc37" },
+        { url: "favicon.svg", revision: "7e840862161341271697daa99a40d76b" },
+        {
+          url: "sounds/timer-tick.mp3",
+          revision: "5fbc2923819f0f1f8b0e12c03179cea5",
+        },
+        {
+          url: "sounds/timer-end.mp3",
+          revision: "abef5396ebc02ca00848f92d6cd1e6c6",
+        },
+        {
+          url: "sounds/night-ambience.mp3",
+          revision: "db96d1db010d11de4e3a784c7cb349fc",
+        },
+        {
+          url: "fonts/inter-vietnamese.woff2",
+          revision: "9df17551da76cba6ee2d5d35fc762ed9",
+        },
+        {
+          url: "fonts/inter-latin.woff2",
+          revision: "260c81a4759baf163c025001c4f27872",
+        },
+        {
+          url: "fonts/inter-latin-ext.woff2",
+          revision: "1ad231aac0a8a891b8374aa5526a5813",
+        },
+        {
+          url: "fonts/bebas-neue-latin.woff2",
+          revision: "2dd698f2699a5ef991625825011bff90",
+        },
+        {
+          url: "fonts/bebas-neue-latin-ext.woff2",
+          revision: "089e175e5cfe9e7e2a0a719196038fb9",
+        },
+        { url: "assets/workbox-window.prod.es5-Bq4GJJid.js", revision: null },
+        { url: "assets/index-XPB6ZU3v.js", revision: null },
+        { url: "assets/index-DmGU9EMM.css", revision: null },
+        {
+          url: "manifest.webmanifest",
+          revision: "472d9d354c8e34d3947636d64100d3a2",
+        },
+      ],
+      {},
+    ),
+    e.cleanupOutdatedCaches(),
+    e.registerRoute(
+      new e.NavigationRoute(e.createHandlerBoundToURL("index.html")),
+    ));
+});
